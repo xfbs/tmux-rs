4257ef79ceb14bc13e3249f012841b635d82d547
Collin Richards <richardscollin@gatech.edu>
2025-03-23 00:18:53 +0800

    remove unused files

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xe
-# export RUSTFLAGS=-Zsanitizer=address
-export RUSTFLAGS="-Zsanitizer=address -C link-arg=-fsanitize=address"
-
-export CC=clang
-rm -f tmux
-cargo build -vvv --lib
-make
